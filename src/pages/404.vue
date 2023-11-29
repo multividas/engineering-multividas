7e6dc834d0897fa11348e25a93ced2f5dbc1c87f
soulaimaneyahya <soulaimaneyh@gmail.com>
2023-11-29 16:40:12 +0100

    fixing 🛠️

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,16 +1,16 @@
 <template>
-  <MainLayout>
-    <div class="text-center my-5">
-        <h1 class="text-dark">Not Found Error | 404</h1>
-        <h5 class="text-muted">Oops! The page you are looking for could not be found.</h5>
+  <Layout>
+    <div class="text-center my-5 py-5">
+      <h1 class="text-dark">Not Found Error | 404</h1>
+      <h5 class="text-muted">Oops! The page you are looking for could not be found.</h5>
     </div>
-  </MainLayout>
+  </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: "404 | Not Found",
-  },
-};
+    title: '404 | Not Found'
+  }
+}
 </script>
